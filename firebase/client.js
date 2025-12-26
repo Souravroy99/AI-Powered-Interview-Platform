c2a5d54e8e0ff4e38c4b76c15de42cb3e96a0cb5
Souravroy99 <souravroy200418@gmail.com>
2025-12-26 11:50:33 +0530

    transfer all the firebase keys from client.js file to .env

--- firebase/client.js
+++ firebase/client.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDRC0bmuCO-DsAVxJZ87EOpyo0nxTt3A9Y",
-  authDomain: "new-aiview.firebaseapp.com",
-  projectId: "new-aiview",
-  storageBucket: "new-aiview.firebasestorage.app",
-  messagingSenderId: "679303457970",
-  appId: "1:679303457970:web:b70f31d68dc4803f942dab",
-  measurementId: "G-6ZYW9K7Z9P"
+  apiKey: process.env.FIREBASE_CONFIG_apiKey,
+  authDomain: process.env.FIREBASE_CONFIG_authDomain,
+  projectId: process.env.FIREBASE_CONFIG_projectId, 
+  storageBucket: process.env.FIREBASE_CONFIG_storageBucket,
+  messagingSenderId: process.env.FIREBASE_CONFIG_messagingSenderId,
+  appId: process.env.FIREBASE_CONFIG_appId,
+  measurementId: process.env.FIREBASE_CONFIG_measurementId
 };
 
 // Initialize Firebase
